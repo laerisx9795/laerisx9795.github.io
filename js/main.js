7d162b8c69df1f99c616447bd5b08fbe952dfa2d
laerisx9795 <laerisx9795@gmail.com>
2017-01-14 17:12:22 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 console.log("1");
 function Hello(){
-  var userName =var userName = $("input#userNameIn").val();
+  var userName = $("input#userNameIn").val();
   alert("Hello,"+userName);
   console.log("2");
 }

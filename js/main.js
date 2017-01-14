5262a5d35739bdf26a71074f50f0aa24590f23ff
laerisx9795 <laerisx9795@gmail.com>
2017-01-14 17:08:53 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,9 +2,11 @@
 
 //alert("Hello,"+userName);
 
+console.log("1");
 function Hello(){
   var name =$("input#a").val();
   alert("Hello,"+name);
+  console.log("2");
 }
 
 $("#BT").on("click", Hello);

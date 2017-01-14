58ff930a768d0c8ab297d145abe2d0ebac263c0f
laerisx9795 <laerisx9795@gmail.com>
2017-01-14 17:15:53 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
-console.log("1");
+updateNum();
+//console.log("1");
 function Hello(){
   var userName = $("input#userNameIn").val();
   alert("Hello,"+userName);
-  console.log("2");
+  //console.log("2");
+  updateNum();
 }
 
 $("#signUp").on("click", Hello);
+
+function isEnterPressed(event){
+  //alert(event.which);  Ascii Code
+  if(event.which == 13){
+    Hello();
+  }
+  updateNum();
+}
+
+$("input#userNameIn").on("keypress",isEnterPressed);
